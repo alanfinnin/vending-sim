5f6d484575599f44fb6d9a10c65ffef774402dd4
Alan Finnin <37178050+alanfinnin@users.noreply.github.com>
2018-11-19 16:32:51 +0000

    File reading to arraylist
    Currently doesnt work for coins (currently void) until quantity of coins is decided on

---
+++ FileReading.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+public class FileReading
+{
+	private double price;
+	private String description;
+	private int quantity;
+	
+	private String coinDescription;
+	private double value;
+	private int coinQuantity;
+		
+	private String username;
+	private String password;
+	private String userTypeCode;
+	
+	private String line;
+	private String[] lines;
+	
+	public ArrayList readFromStockFile() throws IOException
+	{
+		ArrayList<LineItem> products = new ArrayList<LineItem>();
+		File stockFile = new File("Stock.txt");
+		Scanner in = new Scanner(stockFile);
+		
+		try
+		{
+			while(in.hasNextLine())
+			{
+				lines = in.nextLine().split(",");
+				price = Double.parseDouble(lines[0]);
+				description = lines[1];
+				quantity = Integer.parseInt(lines[2]);
+				products.add(new LineItem(new Product(description, price), quantity));
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error: parsing error in Stock file");
+		}
+		return products;
+	}
+	public void readFromMoneyFile() throws IOException
+	{
+		CoinSet set = new CoinSet();
+		File stockFile = new File("Money.txt");
+		Scanner in = new Scanner(stockFile);
+		
+		try
+		{
+			while(in.hasNextLine())
+			{
+				lines = in.nextLine().split(",");
+				coinDescription = lines[0];
+				value = Double.parseDouble(lines[1]);
+				coinQuantity = Integer.parseInt(lines[2]);
+				
+				//set.addCoin(new Coin(value, coinDescription), quantity ??);
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error: parsing error in Money file");
+		}
+		//return set;
+	}
+	public ArrayList readFromAdminFile() throws IOException
+	{
+		ArrayList<String> admins = new ArrayList<String>();
+		File stockFile = new File("Admin.txt");
+		Scanner in = new Scanner(stockFile);
+		
+		try
+		{
+			while(in.hasNextLine())
+			{
+				lines = in.nextLine().split(",");
+				username = lines[0];
+				password = lines[1];
+				userTypeCode = lines[2];
+				admins.add(username + "," + password + "," + userTypeCode);
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error: parsing error in Admin file");
+		}
+		return admins;
+	}
+}
