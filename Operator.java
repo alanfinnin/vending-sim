d64e0b200ae680669314e7c4d85cc1b00545aeeb
Willc200 <45199953+Willc200@users.noreply.github.com>
2018-11-26 13:48:14 +0000

    Updated Operator.java
    Added in constructors .add so contents of constructor get added to Arraylist.
    Added getters and setters.
    Added getList method to get contents of Arraylist.
    getAccount method --> Checks if account is in Arraylist or not(Works)
    addAccount method --> Need to fix this method
    Added toString method.

--- Operator.java
+++ Operator.java
@@ -1,15 +1,70 @@
 public class Operator {
-	protected static Operator currentOperator;
-	private String type;
-	private int code;
+    private String type,t;
+    private String code,c;
+    private static ArrayList<Operator> list = new ArrayList<Operator>();
+
 
 	Operator(){
 		this.type = "Customer";
-		this.code = 0000;
+		this.code = "0000";
+		list.add(this);
 	}
 
-	Operator(String type, int code){
+	Operator(String type, String code){
 		this.type = type;
 		this.code = code;
+		list.add(this);
+		/* if(type=="Admin")
+		{
+			
+		} */
+    }
+    
+    public void setType(String t)
+    {
+        t = type;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+    
+    public void setCode(String c)
+    {
+        c = code;
+    }
+	
+    public String getCode()
+    {
+	return code;
+    }
+    public ArrayList<Operator> getList()
+    {
+        return this.list;
+    }
+	
+    public boolean getAccount(String t, String c)
+    {
+	for(int i = 0; i < list.size(); i++)
+	{
+		if(list.get(i).toString().contains(t+c))
+		{
+			return true;
+		}
 	}
+	return false;
+    }
+	
+    public void addAccount(String t, String c)//NEED TO FIX THIS METHOD
+    {
+	this.t = t;
+	this.c = t;
+	list.add(this);
+    }
+	
+    public String toString()
+    {
+	return getType() + getCode();
+    }
 }
