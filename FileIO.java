b96490e226a3d280d90cde3d465023088736285a
Alan Finnin <37178050+alanfinnin@users.noreply.github.com>
2018-11-29 17:56:59 +0000

    the super for writer and reader

---
+++ FileIO.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class FileIO
+{
+	public static void createFiles() throws IOException
+	{
+		File stockFile = new File("Stock.txt");
+		if(!(stockFile.exists()))
+			stockFile.createNewFile();
+		
+		File moneyFile = new File("Money.txt");
+		if(!(moneyFile.exists()))
+			moneyFile.createNewFile();
+		
+		File adminFile = new File("Operator.txt");
+		if(!(adminFile.exists()))
+			adminFile.createNewFile();
+	}
+}
