24ef5a129e8bf4e755a5a839385fb3ab1c6d57f2
Nytrode <45094007+Nytrode@users.noreply.github.com>
2018-11-30 00:06:17 +0000

    FIFY

--- Operator.java
+++ Operator.java
@@ -1,15 +1,89 @@
+import java.util.ArrayList;
 public class Operator {
-	protected static Operator currentOperator;
 	private String type;
-	private int code;
-
-	Operator(){
-		this.type = "Customer";
-		this.code = 0000;
+    private String code;
+	private String permissions;
+	
+	Operator()
+	{
+		type = "User";
+		code = "0000";
+		permissions = "000";
 	}
-
-	Operator(String type, int code){
+	
+	Operator(String type, String code, String permissions){
 		this.type = type;
 		this.code = code;
+		this.permissions = permissions;
+    }
+
+    public String getPermissions(){
+		return permissions;
+	}
+
+	public void setPermissions(String s){
+		permissions = s;
+	}
+    
+    public void setType(String t)
+    {
+        type = t;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+    
+    public void setCode(String c)
+    {
+        code = c;
+    }
+	
+	public String getCode()
+	{
+		return code;
+	}
+	
+	public boolean canCreateAccount()
+	{
+		if(permissions.substring(0,1).contains("1"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean canAddProduct()
+	{
+		if(permissions.substring(1,2).contains("1"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean canRemove()
+	{
+		if(permissions.substring(2,3).contains("1"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	@Override
+	public String toString()
+	{
+		return type + "," + code;
 	}
 }
