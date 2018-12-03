d206715f410f4c7f32f669de205ce9c9d5732b7e
Daniel Dalton <36508791+ddalton98@users.noreply.github.com>
2018-12-03 19:24:07 +0000

    added javadocs

--- Product.java
+++ Product.java
@@ -1,27 +1,45 @@
 /**
- *
+ * Product object class that stores price and description
+ * @author Daniel Dalton 17219477
  */
 public class Product {
 	private double price;
 	private String description;
 
+	/**
+	 * Constructor for product object
+	 * 
+	 * @param description Description of product (Name)
+	 * @param price Selling price
+	 */
 	Product(String description, double price) {
 		this.description = description;
 		this.price = price;
 	}
 
+	/**
+	 * Getter to retrieve description of product
+	 * 
+	 * @return Description
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Getter to retrieve price of product
+	 * 
+	 * @return Selling price
+	 */
 	public double getPrice() {
 		return price;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
+	/**
+	 * Setter for price of product
+	 * 
+	 * @param price selling price
+	 */
 	public void setPrice(double price) {
 		this.price = price;
 	}
