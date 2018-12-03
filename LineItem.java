094fd3e5f3175838e2677a0ca6848b7a9a5d837e
Willc200 <45199953+Willc200@users.noreply.github.com>
2018-12-03 18:17:15 +0000

    Added Javadocs

--- LineItem.java
+++ LineItem.java
@@ -1,34 +1,57 @@
 class LineItem {
 	private Product product;
 	private int quantity;
-
+	/**
+		* A LineItem
+		*/
 	LineItem(Product prod, int quantity) {
 		this.product = prod;
 		this.quantity = quantity;
 	}
-
+	/**
+	 * Constructs a LineItem with a given description, price and quantity
+	 
+	 * @param description
+	 * @param price
+	 * @param quantity
+	 */
 	LineItem(String description, double price, int quantity) {
 		this.product = new Product(description, price);
 		this.quantity = quantity;
 	}
-
+	/**
+	 * Returns the product
+	 *
+	 * @return product
+	 */
 	public Product getProduct() {
 		return product;
 	}
-
+	/**
+	 * Returns the quantity
+	 *
+	 * @return quantity
+	 */
 	public int getQuantity() {
 		return quantity;
 	}
-
+	/**
+	 * Sets the quantity
+	 *
+	 * @param quantity
+	 */
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-
+	/**
+	 * Decrements the quantity
+	 *
+	 */
 	public void decrementQuantity() {
 		if (quantity >= 1)
 			this.quantity--;
 	}
-
+	
 	@Override
 	public boolean equals(Object o) {
 		if (o instanceof LineItem) {
@@ -43,3 +66,4 @@ class LineItem {
 		return product.toString() + "," + quantity;
 	}
 }
+
